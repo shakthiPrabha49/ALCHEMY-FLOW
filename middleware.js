61a2abe7f63dd17c719ed29d4da3e6706b8fd8c6
Shakthi <shakthipjayathilaka@gmail.com>
2026-05-07 12:43:31 +0530

    pushing

---
+++ middleware.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import { get } from '@vercel/edge-config';
+
+export const config = {
+  matcher: '/welcome',
+};
+
+export async function middleware() {
+  const greeting = await get('greeting');
+
+  return NextResponse.json({
+    message: greeting,
+  });
+}
